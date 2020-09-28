ccf027040b36da0e043c45dbc4240ae10a27ef63
Faiz Mazlan <mfaiz.mazlan@petronas.com>
2020-09-28 19:22:57 +0800

    Added few files in aws-saa-co2/

---
+++ aws-saa-co2/ConvertCsvQuestionsToJson.ps1
@@ -0,0 +1,38 @@
+
+$CsvFile = "QuestionsBank.csv"
+
+$Quizes = Get-Content $CsvFile | ConvertFrom-Csv
+$QuizArray = @()
+$QuestionArray = @()
+
+foreach ($Quiz in $Quizes) {
+    $ChoiceArray = @()
+    $ChoicesName = ($Quiz | Get-Member | ?{ $_.Name -ilike "Choice_*" }).Name
+    foreach ( $ChoiceName in $ChoicesName ){
+        $ChoiceId = $ChoiceName.Split('_')[1] 
+        $ChoiceText = ($Quiz | Select -Property $ChoiceName).$ChoiceName
+        if( $ChoiceText -ne "" ){
+            $ChoiceArray += [PSCustomObject]@{
+                id = $ChoiceId
+                text = $ChoiceText
+            }
+        }
+    }
+    $QuestionArray += [PSCustomObject]@{
+        id = $Quiz.QuestionNum
+        question = $Quiz.Question
+        image = $Quiz.ImageUrl
+        reference = $Quiz.Explaination
+        correct = $Quiz.Answer.Split(" ")
+        choices = $ChoiceArray
+    }
+}
+
+$QuizArray = [PSCustomObject]@{
+    QuizCode = ($Quizes | Select -Unique QuizCode).QuizCode
+    QuizTitle = ($Quizes | Select -Unique QuizTitle).QuizTitle
+    QuizDescription = ($Quizes | Select -Unique QuizDescription).QuizDescription
+    QuizQuestions = $QuestionArray
+}
+
+$QuizArray | ConvertTo-Json -Depth 5
